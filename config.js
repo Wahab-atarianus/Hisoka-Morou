7b443341f9591b9f30293405981f6dc26a73aedb
Wahab-atarianus <109232423+Wahab-atarianus@users.noreply.github.com>
2022-07-17 20:19:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -18,10 +18,10 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
-global.author = 'WhatsApp Bot'
+global.owner = ['6282228217681','6282229217681','6282229217681','6282229217681']
+global.premium = ['6282229217681']
+global.packname = 'Wahab and Inory'
+global.author = 'Zero Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
